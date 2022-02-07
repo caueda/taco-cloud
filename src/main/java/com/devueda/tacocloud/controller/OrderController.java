c49cd7da27f4e840b55e9eb83dd501e3bbfc3ee1
Carlos R. Ueda <carlos.ueda@gmail.com>
2022-02-07 18:24:28 -0400

    OrderController implemented

--- src/main/java/com/devueda/tacocloud/controller/OrderController.java
+++ src/main/java/com/devueda/tacocloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.devueda.tacocloud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Validated Order order) {
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
--- src/main/java/com/devueda/tacocloud/model/Order.java
+++ src/main/java/com/devueda/tacocloud/model/Order.java
@@ -4,18 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Order {
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Street is required")
     private String street;
+    @NotBlank(message = "City is required")
     private String city;
+    @NotBlank(message = "State is required")
     private String state;
+    @NotBlank(message = "Zip is required")
     private String zip;
+    @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])")
     private String ccExpiration;
     private String ccCVV;
 }
